bcc08ac6b9b8cfa50ce69ba9ea81633ccf1a492b
medhi_aberkane <mehdi@mehdi-aberkane.fr>
2019-11-01 18:14:04 +0100

    update script

--- launch.sh
+++ launch.sh
@@ -1,6 +1,12 @@
 service apache2 start
 service mysql start
 
+mysql -uroot -proot -e "create database dvwa;"
+mysql -uroot -proot -e "CREATE USER 'dvwa'@'127.0.0.1' IDENTIFIED BY 'dvwa';"
+mysql -uroot -proot -e "grant all on dvwa.* to 'dvwa'@'127.0.0.1';"
+mysql -uroot -proot -e "flush privileges;"
+
+
 git clone https://github.com/mehdiAberkane/dvwa.git /var/www/html/dvwa
 git clone https://github.com/mehdiAberkane/bwapp.git /var/www/html/bwapp
 
