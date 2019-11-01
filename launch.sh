6727da152ca45fafebca164be0dfae6c4097cd59
medhi_aberkane <mehdi@mehdi-aberkane.fr>
2019-11-01 17:54:19 +0100

    update script

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 service apache2 start
 service mysql start
 
-git clone https://github.com/mehdiAberkane/dvwa.git /var/www/html
-git clone https://github.com/mehdiAberkane/bwapp.git /var/www/html
+git clone https://github.com/mehdiAberkane/dvwa.git /var/www/html/dvwa
+git clone https://github.com/mehdiAberkane/bwapp.git /var/www/html/bwapp
 
