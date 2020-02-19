9962fa96ca8310e3f2e21037bf2ade45b2d45627
Mehdi <mehdi@mehdi-aberkane.fr>
2020-02-19 03:18:05 +0100

    add mitm and dns

--- launch.sh
+++ launch.sh
@@ -9,9 +9,8 @@ mysql -uroot -proot -e "CREATE USER 'dvwa'@'127.0.0.1' IDENTIFIED BY 'dvwa';"
 mysql -uroot -proot -e "grant all on dvwa.* to 'dvwa'@'127.0.0.1';"
 mysql -uroot -proot -e "flush privileges;"
 
-mysql -uroot -proot -e "create database bWAPP;"
-mysql -uroot -proot -e "CREATE USER 'bee'@'127.0.0.1' IDENTIFIED BY 'bug';"
-mysql -uroot -proot -e "grant all on bWAPP.* to 'bee'@'127.0.0.1';"
+mysql -uroot -proot -e "CREATE USER 'bee'@'localhost' IDENTIFIED BY 'bug';"
+mysql -uroot -proot -e "grant all on bWAPP.* to 'bee'@'localhost';"
 mysql -uroot -proot -e "flush privileges;"
 
 
